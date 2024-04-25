c262a06208ca99bed51df58d68bee016747fe87e
alperdegre <alper.degre.dev@gmail.com>
2024-04-25 16:48:08 +0300

    setup cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from typing import Union
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.utils import JWTRequest, create_jwt, create_url_hash, decode_jwt
 from .routers import auth, url
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:4000",
+]
+
+app.add_middleware(
+   CORSMiddleware, 
+   allow_origins=origins,
+   allow_credentials=True,
+   allow_methods=["*"], 
+   allow_headers=["*"],)
 
 app.include_router(auth.router)
 app.include_router(url.router)
